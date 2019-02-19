bdbf3fc88d2a23b7926f40fc266f4ba5fb09a3cf
Hector Diaz <diazhector98@gmai.com>
2019-02-19 10:50:02 -0600

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -12,9 +12,16 @@ app.post('/todos', (req, res) => {
     let todo = new Todo({
         text: req.body.text
     });
-
     todo.save().then((doc) => {
         res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
     }, (e) => {
         res.status(400).send(e);
     });
